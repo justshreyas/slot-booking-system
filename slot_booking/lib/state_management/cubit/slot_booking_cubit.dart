81a7d0b745ccd9c4d525188c0ba946492d8e2ae6
justshreyas <professionally.shreyas@gmail.com>
2024-01-22 07:38:37 +0530

    REFACTOR : cubit methods

--- slot_booking/lib/state_management/cubit/slot_booking_cubit.dart
+++ slot_booking/lib/state_management/cubit/slot_booking_cubit.dart
@@ -6,18 +6,57 @@ import 'package:slot_booking/models/booked_time_slot.dart';
 import 'package:slot_booking/models/time_slot.dart';
 
 import '../../models/user.dart';
+import '../../usecases/book_slot.dart';
+import '../../usecases/fetch_slots.dart';
 
 part 'slot_booking_state.dart';
 
 class SlotBookingCubit extends Cubit<SlotBookingState> {
-  SlotBookingCubit() : super(SlotBookingInitial());
+  final BookSlotUsecase bookSlotUsecase;
+  final FetchSlotsUsecase fetchSlotsUsecase;
 
-  loadSlots(User user) {}
+  SlotBookingCubit(this.bookSlotUsecase, this.fetchSlotsUsecase)
+      : super(SlotBookingInitial());
+
+  loadSlots(User user) async {
+    final result = await fetchSlotsUsecase.call(user);
+
+    if (result != null && result.isNotEmpty) {
+      emit(SlotBookingLoadedSlots(result, null));
+    } else {
+      emit(SlotBookingBookingFailure("Could not load available slots"));
+    }
+  }
 
   bookSlot(
     TimeSlot slot,
     User user,
-  ) {}
+  ) async {
+    final result = await bookSlotUsecase.call(slot, user);
+    if (result != null) {
+      emit(SlotBookingBookingSuccess(result));
+    } else {
+      emit(SlotBookingBookingFailure("Could not book the selected slot"));
+    }
+  }
 
-  bool get canBook => false;
+  selectSlot(
+    TimeSlot slot,
+  ) {
+    if (state is SlotBookingLoadedSlots) {
+      final availableSlots = (state as SlotBookingLoadedSlots).availableSlots;
+
+      emit(SlotBookingLoadedSlots(availableSlots, slot));
+    }
+  }
+
+  bool get canBook {
+    if (state is SlotBookingLoadedSlots) {
+      final candidate = (state as SlotBookingLoadedSlots).selectedSlot;
+
+      return candidate != null && candidate.bookable;
+    } else {
+      return false;
+    }
+  }
 }
